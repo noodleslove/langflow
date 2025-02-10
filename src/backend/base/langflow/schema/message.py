51d99e6477760228fb914506c07f7fab9019fdfd
Eddie Ho <noodleslove@icloud.com>
2025-02-10 00:04:34 -0800

    format

--- src/backend/base/langflow/schema/message.py
+++ src/backend/base/langflow/schema/message.py
@@ -24,6 +24,7 @@ from langflow.schema.data import Data
 from langflow.schema.image import Image, get_file_paths, is_image_file
 from langflow.schema.properties import Properties, Source
 from langflow.schema.validators import timestamp_to_str, timestamp_to_str_validator
+from langflow.schema.video import get_video_frames, is_video_file
 from langflow.utils.constants import (
     MESSAGE_SENDER_AI,
     MESSAGE_SENDER_NAME_AI,
